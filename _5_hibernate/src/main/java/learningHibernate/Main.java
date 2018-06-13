f05a444cffedcc6303de17d2b7f3b8199c39daa5
kfigon <kamil.figon@jitsolutions.pl>
2018-06-13 10:15:30 +0200

    hibernate h2 helloworld, xml configuration

--- _5_hibernate/src/main/java/learningHibernate/Main.java
+++ _5_hibernate/src/main/java/learningHibernate/Main.java
@@ -1,12 +1,44 @@
 package learningHibernate;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.util.List;
+
+
 public class Main {
 
-    public static void init() {
-        
-    }
 
     public static void main(String[] args) {
+        try (SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory()) {
 
+            persist(sessionFactory);
+            load(sessionFactory);
+        }
+    }
+
+    private static void load(SessionFactory sessionFactory) {
+        System.out.println("-- loading persons --");
+        Session session = sessionFactory.openSession();
+
+        List<String> persons = session.createQuery("FROM Student").list();
+
+        persons.forEach(System.out::println);
+        session.close();
+    }
+
+    private static void persist(SessionFactory sessionFactory) {
+        Student p1 = Student.builder().imie("Jan").nazwisko("Kowalski").wiek(34).build();
+        Student p2 = Student.builder().imie("Jan").nazwisko("Nowak").wiek(15).build();
+
+        System.out.println("persisting students");
+
+        try (Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            session.save(p1);
+            session.save(p2);
+            session.getTransaction().commit();
+        }
     }
 }
--- _5_hibernate/src/main/java/learningHibernate/Student.java
+++ _5_hibernate/src/main/java/learningHibernate/Student.java
@@ -7,14 +7,29 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 @Getter
 @Setter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity     // jdbc tabela
 public class Student {
 
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+//    @Column(name = "FULL_NAME") // mozna jawnie nadawac nazwy
     private String imie;
     private String nazwisko;
-    private Integer id;
+    private Integer wiek;
+
+    @Override
+    public String toString() {
+        return String.format("%d. %s %s, wiek: %d", id, imie, nazwisko, wiek);
+    }
 }
